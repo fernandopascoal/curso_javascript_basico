7a9027b014a155883e3c47f19b16826116e3a22b
Fernando Pascoal <fernandopascoal@gmail.com>
2021-03-16 23:17:01 -0300

    Aula 1 e 2 - conceitos básicos, arrays e dicionários

---
+++ js/index.js
@@ -0,0 +1,52 @@
+ //------------------TRABALHANDO COM ARRAYS-------------------//
+
+/* var nome = "Fernando Veiga"
+var idade = 28
+
+var frase = "Japão é o melhor time do mundo"
+
+
+console.log ("Bem vindo " + nome)
+console.log ("Sua idade é " + idade + " anos")
+
+console.log(frase.toUpperCase())
+
+var lista = ["maça", "laranja", "banana"]
+
+lista.push("uva") //insere um item no array
+
+console.log(lista)
+
+lista.pop() //exclui o ultimo item do array
+
+console.log (lista.length) //mostra o tamanho da lista
+
+console.log (lista.reverse()) //mostra a array ao contrario
+
+console.log (lista.toString()) //mostra o array como uma string, sem os colxetes
+
+console.log (lista.join(" * ")) // transforma em string e define um separador*/
+
+//----------------------------------FIM--------------------------------------// 
+
+
+//---------------------TRABALHANDO COM DICIONARIO--------------------------//
+
+/*let fruta = {nome: "morango", cor: "vermelho", preco: "3,50"}
+
+console.log(fruta)
+
+let frutas = [{nome: "morango", cor: "vermelho"}, {nome: "banana", cor: "amarela"}, {nome: "jabuticaba", cor: "preta"}]
+
+console.log(frutas)
+
+alert(frutas[1].nome)*/
+
+//-------------------------------FIM--------------------------------------------//
+
+
+
+
+
+
+
